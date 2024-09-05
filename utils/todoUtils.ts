fe6e421fb68b02c35a011e899f882a563d3fb25c
Alexander Jdanov <ajdanov@fispan.com>
2024-09-05 09:37:08 -0700

    created a new util function to markTodoItemAsCompleted

--- utils/todoUtils.ts
+++ utils/todoUtils.ts
@@ -1,7 +1,21 @@
-import { Page } from '@playwright/test';
+import { Page, expect } from '@playwright/test';
 
 // Create a new todo item
 export async function createNewTodoItem(page: Page, todoText: string) {
     await page.fill('.new-todo', todoText);
     await page.press('.new-todo', 'Enter');
 }
+
+// Mark a specific todo item as a completed by list index #
+export async function markTodoItemAsCompleted(page: Page, index: number) {
+    const todoItemLocator = page.locator('.todo-list li');
+    const toggleCheckbox = todoItemLocator.nth(index).locator('input.toggle');
+    
+    // Click the checkbox to mark the todo item as completed
+    await toggleCheckbox.click();
+  
+    // Verify that the item is marked as completed
+    await expect(todoItemLocator.nth(index)).toHaveClass(/completed/);
+  
+    console.log(`Marked todo item at index ${index} as completed.`);
+  }
